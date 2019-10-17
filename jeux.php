8779074c17843bf8474a14896761406512c3572a
xelofox <38127459+xelofox@users.noreply.github.com>
2019-10-17 17:39:59 +0200

    Add files via upload

--- jeux.php
+++ jeux.php
@@ -5,7 +5,9 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <?php include "template/head.php"; ?>
+        <?php include "template/head.php";
+		include "include/jeux_tab.php";	
+		?>
         <title>Alumni ou pas</title>
 	<script type="text/javascript">
 
@@ -31,108 +33,246 @@
 
 	</script>
     </head>
-    <body>
+    
+	
+	<body>
         <?php include "template/header.php"; ?>
 	<?php if($_init==1) ?>	
-	<div class="contenu">
-		<div id="Q1">
-		<form onsubmit="check_answer(this); return false;">
-			<input type="hidden" name="quest" value="1"/>
-			<b><p>1.Qui est le président de l'ENS Rennes ?  </p></b>
-			<input type="radio" name="rep" value="1" id="Q1_1"/><label for="Q1_1">Pascal Mognol</label><br />
-			<input type="radio" name="rep" value="2"id="Q1_2"/><label for="Q1_2">Pascool Moniol</label><br />
-			<input type="radio" name="rep" value="3"id="Q1_3"/><label for="Q1_3">Pascal Moniol</label><br />
+	<div class="contenu" id="the_quizz">
+		<form onsubmit="check_answer(this); return false;" id="quizz">
+			<input type="hidden" name="question" value="1"/>
+			<b><p><span id="num"></span>. <span id="quest"></span>  </p></b>
+			<span id="reponse"></span>
 			<input type="submit" name="sub"/><br />
 		</form>
-		</div>
 		
-		<div id="Q2">
-		<form onsubmit="check_answer(this); return false;">
-			<input type="hidden" name="quest" value="2"/>
-			<b><p>2. Où se situe l'ENS Rennes ?  </p></b>
-			<input type="radio" name="rep" value="1"id="Q2_1"/><label for="Q2_1">A Rennes comme son nom l'indique</label><br />
-			<input type="radio" name="rep" value="2"id="Q2_2"/><label for="Q2_2">A Saint-Jacques de la lande comme son nom ne l'indique pas</label><br />
-			<input type="radio" name="rep" value="3"id="Q2_3"/><label for="Q2_3">A Bruz, dont le "z" ne se prononce pas</label><br />
-			<input type="submit" name="sub"/><br />
-		</form>
-		</div>
-		<script>toggle("Q2");</script>
-		
-		<div id="Q3">
-		<form onsubmit="check_answer(this); return false;">
-			<input type="hidden" name="quest" value="3"/>
-			<b><p>3. Quel est l'identité secrète de Thomas Baroche ? </p></b>
-			<input type="radio" name="rep" value="1"id="Q3_1"/><label for="Q3_1">Babar</label><br />
-			<input type="radio" name="rep" value="2"id="Q3_2"/><label for="Q3_2">Batman</label><br />
-			<input type="radio" name="rep" value="3"id="Q3_3"/><label for="Q3_3">Baroche</label><br />
-			<input type="submit" name="sub"/><br />
-		</form>
-		</div>
-		<script>toggle("Q3");</script>
-		
-		<div id="Q4">
-		<form onsubmit="check_answer(this); return false;">
-			<input type="hidden" name="quest" value="4"/>
-			<b><p>4. Combien y a-t-il de Thomas dans la promo Mktro 2017?</p></b>
-			<input type="radio" name="rep" value="1"id="Q4_1"/><label for="Q4_1">2</label><br />
-			<input type="radio" name="rep" value="2"id="Q4_2"/><label for="Q4_2">3</label><br />
-			<input type="radio" name="rep" value="3"id="Q4_3"/><label for="Q4_3">4</label><br />
-			<input type="submit" name="sub"/><br />
-		</form>
-		</div>
-		<script>toggle("Q4");</script>
-		
-		<div id="Q5">
-		<form onsubmit="check_answer(this); return false;">
-			<input type="hidden" name="quest" value="5"/>
-			<b><p>5. Qui est Rémi Jardot?</p></b>
-			<input type="radio" name="rep" value="1"id="Q5_1"/><label for="Q5_1">Le président du BDE</label><br />
-			<input type="radio" name="rep" value="2"id="Q5_2"/><label for="Q5_2">Le président du BDA</label><br />
-			<input type="radio" name="rep" value="3"id="Q5_3"/><label for="Q5_3">Le président du BDS</label><br />
-			<input type="radio" name="rep" value="4"id="Q5_4"/><label for="Q5_4">Rémi Jardot</label><br />
-			<input type="radio" name="rep" value="5"id="Q5_5"/><label for="Q5_5">Un marchand de tapis</label><br />
-			<input type="submit" name="sub"/><br />
-		</form>
-		</div>
-		<script>toggle("Q5");</script>
-		
-		<div id="Q6">
-		<form onsubmit="check_answer(this); return false;">
-			<input type="hidden" name="quest" value="6"/>
-			<b><p>6. Qui est Clara Amsallem?</p></b>
-			<input type="radio" name="rep" value="1"id="Q6_1"/><label for="Q6_1">Clara Amsallem</label><br />
-			<input type="radio" name="rep" value="2"id="Q6_2"/><label for="Q6_2">Clara Amsallem</label><br />
-			<input type="radio" name="rep" value="3"id="Q6_3"/><label for="Q6_3">Clara Amsallem</label><br />
-			<input type="submit" name="sub"/><br />
-		</form>
-		</div>
-		<script>toggle("Q6");</script>
+	<script>
+		function question(quizz_tab,num){
+			document.getElementById("num").innerHTML=num+1;
+			document.getElementById("quest").innerHTML=quizz_tab[num][0];
+			var k=1;
+			var rep=document.getElementById("reponse");
+			rep.innerHTML="";
+			quizz_tab[num][1].forEach(function(element) {
+				var para = document.createElement("input");
+				para.type="radio";
+				para.name="rep";
+				para.value=k;
+				para.id=num+"_"+k;
+				rep.appendChild(para);
+				rep.innerHTML+="<label for="+para.id+">"+element+"</label><br />";
+				k++;
+			});
+			
 
-		<div id="Q7">
-		<form onsubmit="check_answer(this); return false;">
-			<input type="hidden" name="quest" value="7"/>
-			<b><p>7. Qui est le joueur de babyfoot ayant de long cheveux, étant très adepte du café, ayant une boite colorée pour conserver son repas, portant des
-			pulls colorés uniquement en période de vacances, enseignant à l'ENS et pesant moins de 100kg ?</p></b>
-			<input type="radio" name="rep" value="1"id="Q7_1"/><label for="Q7_1">Hassen Suallah (AKA Hassen_CF)</label><br />
-			<input type="radio" name="rep" value="2"id="Q7_2"/><label for="Q7_2">Roman LGL (AKA Roman)</label><br />
-			<input type="radio" name="rep" value="3"id="Q7_3"/><label for="Q7_3">Nicolas Testard (AKA Testard)</label><br />
-			<input type="submit" name="sub"/><br />
-		</form>
-		</div>
-		<script>toggle("Q7");</script>
+			
+
+		}
 		
+		
+		var quizz_tab=[
+			[
+				"Qui est le président de l'ENS Rennes?",
+				["Pascal Mognol","Pascool Moniol","Pascal Moniol"],
+				"1"
+			],
+			[
+				"Où se situe l'ENS Rennes?",
+				["A Rennes comme son nom l'indique","A Saint-Jacques de la lande comme son nom ne l'indique pas","A Bruz, dont on ne prononce pas le 'z'"],
+				"3"
+			],
+			[
+				"Quel est l'identité secrète de Thomas Baroche ?",
+				["Babar","Batman","Baroche"],
+				"1"
+			],
+			[
+				"Combien y a-t-il de Thomas dans la promo Mktro 2017?",
+				["2","3","4"],
+				"3"
+			],
+			[
+				"Qui est Rémi Jardot?",
+				["Le président du BDE","Le président du BDA","Le président du BDS","Rémi Jardot","Un marchand de tapis"],
+				"5"
+			],
+			[
+				"Qui est Clara Amsallem?",
+				["Clara Amsallem","Clara Amsallem","Clara Amsallem","Clara Amsallem"],
+				"1"
+			],
+			[
+				"Qui est le joueur de babyfoot ayant de long cheveux, étant très adepte du café, ayant une boite colorée pour conserver son repas, portant des pulls colorés uniquement en période de vacances, enseignant à l'ENS et pesant moins de 100kg ?",
+				["Hassen Suallah (AKA Hassen_CF)","Roman LGL (AKA Roman)","Nicolas Testard (AKA Testard)"],
+				"2"
+			],
+			[
+				"Qui est le responsable du club Babyfoot de l'ENS Rennes?",
+				["Il y a un club Babyfoot à l'ENS Rennes?","Le club baby est au BDA","Le club baby est au BDS","Micka (Michael)","Testard"],
+				"1"
+			],
+			[
+				"Quel est le sens de la vie de l'univers et de tout le reste?",
+				["41","42","43"],
+				"2"
+			],
+			[
+				"Que vaut la somme des entiers naturels?",
+				["-1/12","(pi^2)/6","l'infini"],
+				"1"
+			],
+			[
+				"Est-ce que P=NP?",
+				["oui","non","peut-être"],
+				"3"
+			],
+			[
+				"Qui est Cécile Lejeune?",
+				["Une femme","La responsable Communication (emploi fictif?)","Je ne sais pas"],
+				"3"
+			],
+			[
+				"Qui suis-je?",
+				["Personne","Un quizz, respecte mes sentiments s'il te plait","LOL, un quizz ça a pas de sentiments"],
+				"3"
+			],
+			[
+				"Quel est la capital du Tamalou?",
+				["Gébobola","Danmonjambe","Surtomné"],
+				"1"
+			],
+			[
+				"Quelle est la plus grande des ENS?",
+				[
+					"Bouzaréah",
+					"Constantine",
+					"Kouba",
+					"Ulm",
+					"Bousaada",
+					"Porto-Novo",
+					"Natitingou",
+					"Maroua",
+					"Yaoundé",
+					"Lyon",
+					"Côte d'Ivoire",
+					"Est de la Chine",
+					"Libreville",
+					"Port-au-Prince",
+					"Pise",
+					"Ampefiloha, Antananarivo",
+					"Cachan",
+					"Casablanca",
+					"Fès",
+					"Marrakech",
+					"Martil",
+					"Rennes",
+					"Meknès",
+					"Rabat",
+					"Mohammedia",
+					"Nouakchott",
+					"Abdou Moumouni",
+					"Cracovie",
+					"Dakar",
+					"N'Djamena",
+					"Tunis"
+				],
+				"22"
+			]
+		];
+		
+		
+		function BonneReponse() {
+			var msg="Bien vu!";
+			console.log(msg)
+		alert(msg);} 
+		
+		function MauvaiseReponse() {
+			var msg="T'es qu'une merde";
+			console.log(msg)
+		alert(msg);} 
+		
+		var num=0;
+		var Qmax=quizz_tab.length;
+		function check_answer(form){
+			if(form.rep.value==quizz_tab[num][2] || num==6-1){
+				if (num==Qmax-1) {
+					var msg="Bien joué champion! Tu es le meilleur! A voir combien de fois gtu as recommencé maintenant... :)";
+					console.log(msg)
+					alert(msg);
+					document.getElementById("Score").value=num;
+					toggle("the_quizz");
+					toggle("give_pseudo");
+				}
+				else{
+					if (num==0){
+						BonneReponse();
+					}
+					num++;
+					question(quizz_tab,num)
+				}
+			}
+			else{
+				if (num==3){
+					var msg="Tu as oublié Béatrice! (Thomas de son nom)";
+					console.log(msg)
+					alert(msg);
+				}
+				else{
+					MauvaiseReponse();
+				}
+				question(quizz_tab,num);
+				document.getElementById("Score").value=num;
+				num=0;
+				toggle("the_quizz");
+				toggle("give_pseudo");
+			}
+		}
+	
+	
+			
+		
+		question(quizz_tab,num);
+	</script>
+	</div>
+	
+	<div class="contenu" id="give_pseudo">
+		<h3 style="color: blue;"><i>Rentrez votre pseudo</i></h3>
+		<form method="post" action="Log_score.php">
+			<label for="Pseudo">Pseudo :</label> <input type="text" name="Pseudo" id="Pseudo"/><br/>
+			<input type="hidden" name="Score" id="Score"/>
+			<input type="submit" value="clique là"/>
+		</form>
+    </div>
+	<script>toggle("give_pseudo");</script>
+		
+	<div style="text-align:center; width:1px; margin: auto;"><h2 style="color:red;">Historique</h2>
+	<table>
+		<tr>
+			<th>Pseudo</th>
+			<th>Score</th>
+		</tr>
+		<?php
+			$score = get_score();
+			foreach($score as $u) {
+				?>
+				<tr>
+					<td><?php echo htmlentities($u["Pseudo"]); ?></td>
+					<td><?php echo htmlentities($u["Score"]); ?></td>
+				</tr>
+			<?php }
+		?>
+	</table>
+	</div>
+	
 		<div class="contenu">
-		<h1> Cliquer sur la tête de Charlie </h1>		</div>
+		<h1> Cliquer sur la tête de Charlie </h1>		
 		<img src="images/charlie.jpg" alt="charlie" usemap="#charlie" style="width: 1000px; margin-left: auto; margin-right: auto; display: block;">
 		<map name="charlie">
 			<area shape="circle" coords="838.5px,148px,5px" alt="charlie" href="javascript:Message()" href="https://www.google.com">
 		
         </map>
 	</div>
-		
-
-
-	<!-- L'identificateur id du div doit être unique. Içi, "foo" ou "bar" ou autre, mais unique dans le document-->
+	
 	
 	</div>
 		<?php include "template/footer.php"; ?>
@@ -145,68 +285,7 @@
 		alert(msg);} </script>
 		
 		<script>
-		
-		function BonneReponse() {
-			var msg="Bien vu!";
-			console.log(msg)
-		alert(msg);} </script>
-		
-		<script>
-		function MauvaiseReponse() {
-			var msg="T'es qu'une merde";
-			console.log(msg)
-		alert(msg);} </script>
-	
 
-	   
-		
-	<script>function check_answer(form){
-		var quest = form.quest.value;
-		switch (quest) {
-			case '1':
-				var value=1;
-				break;
-			case '2':
-				var value=3;
-				break;
-			case '3':
-				var value=1;
-				break;
-			case '4':
-				var value=3;
-				break;
-			case '5':
-				var value=5;
-				break;
-			case '6':
-				var value=1;
-				break;
-			case '7':
-				var value=2;
-				break;
-		}
-			if(form.rep.value==value || quest=='6'){
-				BonneReponse();
-				toggle("Q"+quest);
-				quest=quest*1+1;
-				toggle("Q"+quest);
-			}
-			else{
-				if (quest=='4'){
-					var msg="Tu as oublié Béatrice! (Thomas de son nom)";
-					console.log(msg)
-					alert(msg);
-				}
-				else{
-					MauvaiseReponse();
-				}
-				toggle("Q"+quest);
-				quest=1;
-				toggle("Q"+quest);
-			}
-	}
-	</script>
-		
 		
  
 
